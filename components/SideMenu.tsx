0c63661a2d36c385f8bd9ac419b4168e1b269d78
chandan7077p <chandansbi007@gmail.com>
2025-07-04 00:03:03 +0530

    Initial commit

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import Logo from './Logo'
+import { X } from 'lucide-react'
 interface SidebarProps {
   isOpen: boolean
   onClose: () => void
@@ -7,11 +9,22 @@ interface SidebarProps {
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
   return (
     <div
-      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full backdrop-blur-sm text-crimson shadow-xl transform transition-transform duration-300 ${
         isOpen ? 'translate-x-0' : '-translate-x-full'
       } hoverEffect`}
     >
-      SideMenu
+      <div className='min-w-full max-w-full bg-white/75 h-screen p-5 border-r border-r-white/20 shadow-2xl flex flex-col gap-6'>
+        <div className='flex items-center justify-between gap-5'>
+          <Logo />
+          <button
+            onClick={onClose}
+            className='hover:text-royal-green hover:cursor-pointer hoverEffect'
+          >
+            <X />
+          </button>
+        </div>
+        <div></div>
+      </div>
     </div>
   )
 }
