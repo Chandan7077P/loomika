644c43885130154412358d1aae93875800888dfb
Chandan7077P <chandansbi007@gmail.com>
2025-07-04 12:08:52 +0530

    Initial commit - BabyMarine website

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,10 +1,9 @@
-// app/About/page.tsx
 import React from "react";
 
 const AboutPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">About Us</h1>
+      <h1 className="text-4xl font-bold">About Us</h1>
     </div>
   );
 };
