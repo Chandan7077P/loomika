cd7b30c505c042606059a6b1a239506b938e3aa7
chandan7077p <chandansbi007@gmail.com>
2025-07-11 02:42:31 +0530

    Loomika Snitch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,40 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Navbar from "./Components/Navbar";
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Navbar from './Components/Navbar'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Baby Marine International",
-  description: "USDA Certified Seafood Processing and Export Company",
-};
+  title: 'Loomika | Online Sambalpuri Store',
+  description: 'Shop Online for Sambalpuri Dress, Kurtis, Sarees and Shirts',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-100 text-black`}
       >
         {/* MOBILE-LIKE CONTAINER */}
-        <div className="mx-auto max-w-[480px] min-h-screen bg-white shadow-md relative overflow-hidden">
+        <div className='mx-auto max-w-[480px] min-h-screen bg-white shadow-md relative overflow-hidden'>
           <Navbar />
-          <main className="pb-24">{children}</main> {/* Extra space for bottom nav if needed */}
-          <footer className="w-full bg-gray-800 text-white text-center py-2 text-sm">
-            <p>
-              BMI Website designed by{" "}
-              <span className="font-semibold">Chandan Padhan</span> – Digital
-              Marketing Intern at{" "}
-              <span className="font-semibold">Highland Group</span>
-            </p>
-          </footer>
+          <main className='pb-24'>{children}</main>{' '}
+          {/* Extra space for bottom nav if needed */}
         </div>
       </body>
     </html>
-  );
+  )
 }
